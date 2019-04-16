e072817e3f9723bca2e92413a36bb571bfdd58d1
Evan Tann <egtann@gmail.com>
2019-04-16 10:13:36 -0700

    add rate limit, should retry logic

--- client.go
+++ client.go
@@ -3,20 +3,25 @@ package retryablehttp
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"sync"
 	"time"
+
+	"golang.org/x/time/rate"
 )
 
 // Client does auto-retries. If passed a logger, it logs the start and end of
 // requests for observability.
 type Client struct {
-	c       *http.Client
-	retries int
-	log     Logger
-	mu      sync.RWMutex
+	c           *http.Client
+	shouldRetry ShouldRetryFn
+	limiter     *rate.Limiter
+	retries     int
+	log         Logger
+	mu          sync.RWMutex
 }
 
 type Logger interface {
@@ -24,7 +29,7 @@ type Logger interface {
 }
 
 func NewClient(client *http.Client) *Client {
-	return &Client{c: client}
+	return &Client{c: client, shouldRetry: defaultRetrier}
 }
 
 func (c *Client) WithLogger(log Logger) *Client {
@@ -46,6 +51,36 @@ func (c *Client) WithRetries(r int) *Client {
 	return c
 }
 
+func (c *Client) WithLimiter(l *rate.Limiter) *Client {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.limiter = l
+	return c
+}
+
+// ShouldRetryFn allows specifying a custom function which should return true
+// if a retry should be done, or false if no retries should be done. This
+// allows you to handle specific status codes differently.
+type ShouldRetryFn func(resp *http.Response) bool
+
+func defaultRetrier(resp *http.Response) bool {
+	if resp.StatusCode >= 400 && resp.StatusCode < 500 {
+		return true
+	}
+
+	// Also retry on reverse proxy issues
+	return resp.StatusCode == http.StatusBadGateway
+}
+
+func (c *Client) WithShouldRetry(fn ShouldRetryFn) *Client {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.shouldRetry = fn
+	return c
+}
+
 func (c *Client) Do(req *http.Request) (*http.Response, error) {
 	if c.retries == 0 {
 		return c.do(req)
@@ -61,7 +96,7 @@ func (c *Client) Do(req *http.Request) (*http.Response, error) {
 	if req.Body != nil {
 		byt, err = ioutil.ReadAll(req.Body)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("read body: %s", err.Error())
 		}
 	}
 	header := req.Header
@@ -72,7 +107,7 @@ func (c *Client) Do(req *http.Request) (*http.Response, error) {
 			time.Sleep(time.Second * time.Duration(i+1))
 			continue
 		}
-		if retryable(rsp.StatusCode) {
+		if c.shouldRetry(rsp) {
 			rsp.Body.Close()
 			time.Sleep(time.Second * time.Duration(i+1))
 			continue
@@ -91,7 +126,7 @@ func (c *Client) retry(
 	rdr := bytes.NewReader(byt)
 	req, err := http.NewRequest(req.Method, req.URL.String(), rdr)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("new request: %s", err.Error())
 	}
 	req = req.WithContext(ctx)
 	req.Header = header
@@ -103,7 +138,7 @@ func (c *Client) do(req *http.Request) (*http.Response, error) {
 		// Remove basic auth info before logging
 		ul, err := url.Parse(req.URL.String())
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("parse url: %s", err.Error())
 		}
 		ul.User = nil
 		urlStr := ul.String()
@@ -111,6 +146,11 @@ func (c *Client) do(req *http.Request) (*http.Response, error) {
 		// Log start and end times for observability
 		c.log.Printf("start: http: %s %s", req.Method, urlStr)
 		defer c.log.Printf("end: http: %s %s", req.Method, urlStr)
+	}
+	if c.limiter != nil {
+		if err := c.limiter.Wait(req.Context()); err != nil {
+			return nil, fmt.Errorf("wait: %s", err.Error())
+		}
 	}
 	return c.c.Do(req)
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module github.com/egtann/retryablehttp
+
+go 1.12
+
+require golang.org/x/time v0.0.0-20190308202827-9d24e82272b4
